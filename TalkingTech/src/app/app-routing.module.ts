19cf0fba2729d3cf35fd896cb8f455ea03472a42
Juan Guerrero <jcamiloguerrero@gmail.com>
2020-09-04 17:01:30 -0500

    Module and component of page-not-found is added and routing and configuration for redirection are added

--- TalkingTech/src/app/app-routing.module.ts
+++ TalkingTech/src/app/app-routing.module.ts
@@ -21,6 +21,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import ('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: '**',
+    loadChildren: () => import ('./page-not-found/page-not-found.module').then(m => m.PageNotFoundModule)
   }
 ];
 
