88f18261e21b591b713bec2a8222e6b507887032
Juan Guerrero <jcamiloguerrero@gmail.com>
2020-09-29 22:22:49 -0500

    Post detail component is generated and failure alert message is modified in category creation

--- TalkingTech/src/app/home/home-routing.module.ts
+++ TalkingTech/src/app/home/home-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
+import { PostsDetailComponent } from './components/posts-detail/posts-detail.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'detail',
+    component: PostsDetailComponent
   }
 ];
 
--- TalkingTech/src/app/home/home.module.ts
+++ TalkingTech/src/app/home/home.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './components/home/home.component';
+import { PostsDetailComponent } from './components/posts-detail/posts-detail.component';
 
 import { SharedModule } from '@shared/shared.module';
 import { MaterialModule } from '@material/material.module';
@@ -11,7 +12,7 @@ import { NgxSpinnerModule } from 'ngx-spinner';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, PostsDetailComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
