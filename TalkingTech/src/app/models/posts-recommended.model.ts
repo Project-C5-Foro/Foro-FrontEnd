d3c9ac69d5ea40d5a46667b4f01dc47a5f653f6d
Juan Guerrero <jcamiloguerrero@gmail.com>
2020-10-08 23:59:25 -0500

    fixe bugs and deploy

--- TalkingTech/src/app/models/posts-recommended.model.ts
+++ TalkingTech/src/app/models/posts-recommended.model.ts
@@ -1,15 +1,25 @@
 export interface Recommended {
     id: number;
     post_id: {
-        id: number;
         created: string;
         modified: string;
-        title: string;
         post: string;
         status: boolean
         visited: number;
-        user: number;
-        category: number;
+        user: {
+            id: number,
+            username: string;
+            first_name: string;
+            last_name: string;
+            email: string;
+            is_superuser: boolean;
+            is_client: boolean;
+         };
+        category: {
+            id: number;
+            category: string;
+            status: boolean;
+         };
         };
     created: boolean;
     modified: boolean;
